c4d4f16ffd03973c9c51e4d429817ba0a17215a4
dlovell <dlovell@gmail.com>
2014-07-11 13:59:15 -0400

    remove uncessary sections

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-import re
 import os
-import sys
 
-vi = sys.version_info
-(major, minor) = (vi[0], vi[1])
-if major != 2 or minor not in (6, 7):
-    raise Exception("Enversion requires either Python 2.6 or 2.7.")
-
-try:
-    from svn import core, fs, ra
-except ImportError:
-    msg = "Enversion requires Subversion's Python bindings to be installed."
-    raise Exception(msg)
 
 try:
     import ez_setup
@@ -27,14 +15,6 @@ except ImportError:
     has_setuptools = False
     from distutils.core import setup
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-def join_path(*args):
-    return os.path.abspath(os.path.normpath(os.path.join(*args)))
-
-def read(*parts):
-    with open(join_path(basedir, *parts), 'r') as f:
-        return f.read()
 
 def find_packages(base):
     packages = []
@@ -46,18 +26,9 @@ def find_packages(base):
                 packages.append(fragment.replace(os.sep, '.'))
     return packages
 
-def version():
-    return (
-        re.compile(r"^__version__ = '(.*?)'$", re.S)
-          .match(read('lib', 'evn', '__init__.py'))
-          .group(1)
-    )
-
 def run_setup():
     setup(
         name='enversion',
-        version=version(),
-        license='Apache',
         description='Enterprise Subversion Framework',
         author='Trent Nelson',
         author_email='trent@snakebite.org',
@@ -65,28 +36,11 @@ def run_setup():
         keywords='subversion,svn,scm,pysvn',
         packages=find_packages('lib'),
         package_dir={'': 'lib'},
-        entry_points={
-            'console_scripts': [
-                'evnadmin = evn.admin.cli:main',
-            ],
-        },
-        classifiers=[
-            'Environment :: Console',
-            'License :: OSI Approved :: Apache Software License',
-            'Development Status :: 3 - Alpha',
-            'Operating System :: POSIX',
-            'Operating System :: MacOS :: MacOS X',
-            'Operating System :: Microsoft :: Windows',
-            'Intended Audience :: Developers',
-            'Intended Audience :: System Administrators',
-            'Intended Audience :: Configuration Managers',
-            'Intended Audience :: Information Technology',
-            'Programming Language :: Python',
-            'Programming Language :: Unix Shell',
-            'Topic :: Software Development :: Quality Assurance',
-            'Topic :: Software Development :: Version Control',
-            'Topic :: Software Development :: Version Control :: Subversion',
-        ],
+#        entry_points={
+#            'console_scripts': [
+#                'evnadmin = evn.admin.cli:main',
+#            ],
+#        },
     )
 
 if __name__ == '__main__':
